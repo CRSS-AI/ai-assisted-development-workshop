f2f8270f7f0fa6b4fd640c225767922ada2f5fb8
Giuliana Ortega Saborío <giuliana.ortega@costaricasoftwareservices.com>
2025-06-27 03:12:11 +0000

    created ai instance in foundry and created default project

---
+++ demo-github-copilot/file.js
@@ -0,0 +1,21 @@
+import { AzureChatOpenAI } from "@langchain/openai";
+
+const llm = new AzureChatOpenAI({
+  model: "gpt-4.1-mini",
+  temperature: 0,
+  maxTokens: undefined,
+  maxRetries: 2,
+  azureOpenAIApiKey: process.env.AZURE_OPENAI_API_KEY, // In Node.js defaults to process.env.AZURE_OPENAI_API_KEY
+  azureOpenAIApiInstanceName: process.env.AZURE_OPENAI_API_INSTANCE_NAME, // In Node.js defaults to process.env.AZURE_OPENAI_API_INSTANCE_NAME
+  azureOpenAIApiDeploymentName: process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME, // In Node.js defaults to process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME
+  azureOpenAIApiVersion: process.env.AZURE_OPENAI_API_VERSION, // In Node.js defaults to process.env.AZURE_OPENAI_API_VERSION
+});
+
+const aiMsg = await llm.invoke([
+  [
+    "system",
+    "You are a helpful assistant that translates English to French. Translate the user sentence.",
+  ],
+  ["human", "I love programming."],
+]);
+aiMsg;
